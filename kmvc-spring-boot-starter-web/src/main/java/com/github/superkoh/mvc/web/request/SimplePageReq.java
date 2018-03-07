754040d5e33961fe340ffe3c475bcb4626685f62
王乐天 <koh.718@gmail.com>
2018-03-07 18:05:13 +0800

    1.0.6: bug fix

--- kmvc-spring-boot-starter-web/src/main/java/com/github/superkoh/mvc/web/request/SimplePageReq.java
+++ kmvc-spring-boot-starter-web/src/main/java/com/github/superkoh/mvc/web/request/SimplePageReq.java
@@ -1,6 +1,7 @@
 package com.github.superkoh.mvc.web.request;
 
 import com.github.superkoh.mvc.lang.BaseObject;
+import com.github.superkoh.mvc.type.Page;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.NotNull;
@@ -18,4 +19,8 @@ public class SimplePageReq extends BaseObject {
   @NotNull
   @ApiModelProperty("页码")
   private Integer pageNo = 1;
+
+  public Page toPage() {
+    return new Page(pageSize, pageNo);
+  }
 }
