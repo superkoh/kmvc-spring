cd59c32743fa66f0fae764524976bb15adbde010
王乐天 <koh.718@gmail.com>
2018-04-26 18:45:10 +0800

    fix DTOTransferable interface

--- kmvc-spring-boot-starter-web/src/main/java/com/github/superkoh/mvc/web/request/DTOTransferable.java
+++ kmvc-spring-boot-starter-web/src/main/java/com/github/superkoh/mvc/web/request/DTOTransferable.java
@@ -7,14 +7,18 @@ import org.springframework.beans.BeanUtils;
 public interface DTOTransferable<T> {
 
   default T toDTO() {
-    Type type = getClass().getGenericSuperclass();
-    ParameterizedType parameterizedType = (ParameterizedType) type;
-    Class<T> clazz = (Class<T>) parameterizedType.getActualTypeArguments()[0];
-    try {
-      T bean = clazz.newInstance();
-      BeanUtils.copyProperties(this, bean);
-      return bean;
-    } catch (InstantiationException | IllegalAccessException ignored) {
+    Type[] types = this.getClass().getGenericInterfaces();
+    for (Type type : types) {
+      ParameterizedType parameterizedType = (ParameterizedType) type;
+      if (parameterizedType.getRawType().equals(DTOTransferable.class)) {
+        Class<T> clazz = (Class<T>) parameterizedType.getActualTypeArguments()[0];
+        try {
+          T bean = clazz.newInstance();
+          BeanUtils.copyProperties(this, bean);
+          return bean;
+        } catch (InstantiationException | IllegalAccessException ignored) {
+        }
+      }
     }
     return null;
   }
